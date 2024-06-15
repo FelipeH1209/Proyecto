512727b3499bc60a51162401b9f085b20475f11c
Jerson <infopae.soporte03@gmail.com>
2024-06-14 23:44:56 -0500

    Ajustes login

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -8,36 +8,112 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email = '', _password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.all(20),
           children: [
-            Image.asset(
-              'assets/images/vacas.jpg',
-              height: 300,
+            Text(
+              "Aplicación ganadería",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            TextField(
-              enableInteractiveSelection: false,
-              keyboardType: TextInputType.name,
-              decoration: InputDecoration(labelText: "Nombre Usuario"),
+            SizedBox(height: 50),
+            Center(
+              child: Container(
+                width: 170,
+                height: 170,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset(
+                  'assets/images/logo_login.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            TextFormField(
+              key: const Key('login-email'),
+              onChanged: (text) {
+                _email = text.trim();
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                label: Text('Email'),
+              ),
+              validator: (text) {
+                text = text ?? '';
+                final isValid = RegExp(
+                        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                    .hasMatch(text);
+                if (isValid) {
+                  return null;
+                }
+                return 'Invalid email';
+              },
             ),
             SizedBox(height: 30),
-            TextField(
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              key: const Key('login-pass'),
+              onChanged: (value) {
+                _password = value.replaceAll(' ', '');
+              },
               obscureText: true,
-              enableInteractiveSelection: false,
-              decoration: InputDecoration(labelText: "Contraseña"),
+              decoration: const InputDecoration(
+                label: Text('Contraseña'),
+              ),
+              validator: (value) {
+                value = value ?? '';
+                if (value.length >= 8) {
+                  return null;
+                }
+                return 'Invalid password';
+              },
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+            Builder(builder: (context) {
+              return ElevatedButton(
+                onPressed: () => _submit(context),
+                child: const Text(
+                  'Ingresar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.indigo.shade400,
+                  elevation: 10,
+                ),
+              );
+            })
           ],
         ),
       ),
     );
+  }
+
+  void _submit(BuildContext context) {
+    final formState = context.findAncestorStateOfType<FormState>();
+    if (formState!.validate()) {
+      print('hello pass');
+    }
+
+    // if (_formKey.currentState!.validate()) {
+    //   // send form request
+    // }
   }
 }
