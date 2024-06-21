456488708c0ef84f9845e96cdccb302e87d4d660
Jerson <infopae.soporte03@gmail.com>
2024-06-20 21:09:30 -0500

    nuevos directorios

---
+++ lib/database/user_dao.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_application_ganaderia/database/database_helper.dart';
+import 'package:flutter_application_ganaderia/models/user_model.dart';
+
+class UserDao {
+  final database = DatabaseHelper.instance.db;
+
+  Future<List<UserModel>> readAll() async {
+    final data = await database.query('users');
+    return data.map((e) => UserModel.fromMap(e)).toList();
+  }
+
+  Future<int> insert(UserModel user) async {
+    return await database
+        .insert('users', {'name': user.name, 'email': user.email});
+  }
+
+  Future<void> update(UserModel user) async {
+    await database
+        .update('users', user.toMap(), where: 'id = ?', whereArgs: [user.id]);
+  }
+
+  Future<void> delete(UserModel user) async {
+    await database.delete('users', where: 'id = ?', whereArgs: [user.id]);
+  }
+}
